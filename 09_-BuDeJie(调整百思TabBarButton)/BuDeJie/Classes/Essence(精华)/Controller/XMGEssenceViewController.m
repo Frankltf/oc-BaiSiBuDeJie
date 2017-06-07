d66b046de937a404fe7608689cba989f8f1f7fae
tengfei <1536303470@qq.com>
2017-06-07 16:33:12 +0800

    项目2

---
+++ 09_-BuDeJie(调整百思TabBarButton)/BuDeJie/Classes/Essence(精华)/Controller/XMGEssenceViewController.m
@@ -0,0 +1,38 @@
+//
+//  XMGEssenceViewController.m
+//  BuDeJie
+//
+//  Created by xiaomage on 16/3/11.
+//  Copyright © 2016年 小码哥. All rights reserved.
+//
+
+#import "XMGEssenceViewController.h"
+
+@interface XMGEssenceViewController ()
+
+@end
+
+@implementation XMGEssenceViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.view.backgroundColor = [UIColor redColor];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
