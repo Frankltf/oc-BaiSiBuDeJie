5c8d2d056891c06229b077e7867475171acbc47a
tengfei <langerfei@163.com>
2017-06-11 17:02:28 +0800

    文件

---
+++ BuDeJie/BuDeJie/Classes/FriendTrend/Controller/XMGFriendTrendViewController.h
@@ -0,0 +1,13 @@
+//
+//  XMGFriendTrendViewController.h
+//  BuDeJie
+//
+//  Created by 刘腾飞 on 2017/6/11.
+//  Copyright © 2017年 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XMGFriendTrendViewController : UIViewController
+
+@end
