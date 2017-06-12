3252a7665520364cb0b984e07e936fbdf7bb1723
tengfei <1536303470@qq.com>
2017-06-12 10:57:05 +0800

    优化

---
+++ BuDeJie/BuDeJie/Classes/Other/Category/UIImage+Image.m
@@ -0,0 +1,13 @@
+//
+//  UIImage+Image.m
+//  BuDeJie
+//
+//  Created by admin on 2017/6/12.
+//  Copyright © 2017年 admin. All rights reserved.
+//
+
+#import "UIImage+Image.h"
+
+@implementation UIImage (Image)
+
+@end
