835a8c287182bb045e80d1fbe9ea52839ae0ba05
tengfei <1536303470@qq.com>
2017-06-12 15:51:32 +0800

    back

--- BuDeJie/BuDeJie/Classes/Main/XMGNavigationController.m
+++ BuDeJie/BuDeJie/Classes/Main/XMGNavigationController.m
@@ -23,6 +23,8 @@
         viewController.hidesBottomBarWhenPushed=YES;
         UIButton *backButton=[UIButton buttonWithType:UIButtonTypeCustom];
         [backButton setTitle:@"返回" forState:UIControlStateNormal];
+        [backButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+        [backButton setTitleColor:[UIColor redColor] forState:UIControlStateHighlighted];
         [backButton setImage:[UIImage imageNamed:@"navigationButtonReturn"] forState:UIControlStateNormal];
         [backButton setImage:[UIImage imageNamed:@"navigationButtonReturnClick"] forState:UIControlStateHighlighted];
         [backButton sizeToFit];
